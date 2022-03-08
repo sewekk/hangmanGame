300b2332e6e8ee6df00096132411d312a6b1c091
Sewek <seweryq@gmail.com>
2022-03-08 22:16:45 +0100

    created responsive design of app with media queries and javascript

--- app/index.js
+++ app/index.js
@@ -1,3 +1,13 @@
 import './index.scss';
 
-console.log('Boilerplate is working!');
+const letters = document.querySelectorAll('.word__letter');
+const lettersContainerWidth = document.querySelector('.word').offsetWidth;
+
+const widthOfLetter = (lettersContainerWidth / letters.length) - 10;
+
+console.log(widthOfLetter);
+letters.forEach(item => {
+    item.style.width = `${widthOfLetter}px`;
+    item.style.height = `${widthOfLetter}px`;
+})
+
