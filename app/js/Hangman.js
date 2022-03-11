059cda5e55ea1d8668490bfcc8962234ff2dd0b0
Sewek <seweryq@gmail.com>
2022-03-11 01:35:33 +0100

    created chekIfWordWontainsLetter, checkIfWin, listenerOnKeyDownEvent methods

--- app/js/Hangman.js
+++ app/js/Hangman.js
@@ -1,20 +1,23 @@
 class Hangman {
 	constructor() {
 		this.word = 'siemaa';
+		this.guessedWord = [];
 		this.missed = [];
 		this.apiKey = 'hello';
 		this.apiUrl = 'world';
 		this.letterContainer = document.querySelector('.word');
 		this.letters;
+		this.ifWin = false;
 
 		this.getWord(this.apiKey, this.apiUrl);
-	}
+		this.listenerOnKeyDownEvent();
+	};
 
-	getWord(apiKey, apiUrl) {
+	getWord = (apiKey, apiUrl) => {
 		this.createWordTiles(this.word.length);
 	}
 
-	createWordTiles(wordLenght) {
+	createWordTiles = (wordLenght) => {
 		for (let i = 0; i < wordLenght; i++) {
 			const tile = document.createElement('div');
 			tile.classList.add('word__letter');
@@ -26,6 +29,7 @@ class Hangman {
 
 	displayWordTiles = () => {
 		this.letters = document.querySelectorAll('.word__letter');
+		this.letters.innerHTML = "";
 		const lettersContainerWidth = this.letterContainer.offsetWidth;
 		let widthOfLetter = (lettersContainerWidth / this.letters.length) - 10;
 
@@ -40,6 +44,49 @@ class Hangman {
 			item.style.height = `${widthOfLetter}px`;
 		});
 	};
+
+	listenerOnKeyDownEvent = () => {
+		document.addEventListener('keydown', (e) => {
+			this.currentLetter = e.code.slice(-1).toLowerCase();
+			this.checkIfWordContainsLetter();
+		})
+	};
+
+	checkIfWordContainsLetter = () => {	
+		let ifGuessed = false;
+		[...this.word].forEach((letter, index) => {
+			if(letter === this.currentLetter){
+				this.guessedWord[index] = this.currentLetter;
+				console.log(index);
+				ifGuessed = true;
+			};
+		});
+
+		if(ifGuessed === false){
+			this.missed.push(this.currentLetter);
+			this.displayMissed();
+		}
+		
+		const checkIfWin = this.checkIfWin();
+		if(checkIfWin === ture){
+			this.displayWinnerWindow()
+		}
+	};
+
+	checkIfWin = () => {
+		if(this.word === this.guessedWord.join('')){
+			return true;
+		} 
+		else{
+			return false;
+		} 
+	}
+
+	displayWinnerWindow = () => {
+		console.log('winner!');
+	}
+
+
 
 }
 export default Hangman;
